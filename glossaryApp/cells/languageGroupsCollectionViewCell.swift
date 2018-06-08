ded03c2155d7ccc10ebb0d01deedab0d68d03317
Wanaka <jonas.haag@hotmail.com>
2018-06-08 14:05:51 +0200

    Add group translation and add to list in db

---
+++ glossaryApp/cells/languageGroupsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  languageGroupsCollectionViewCell.swift
+//  glossaryApp
+//
+//  Created by Jonas Haag on 2018-06-08.
+//  Copyright © 2018 Jonas Haag. All rights reserved.
+//
+
+import UIKit
+
+class languageGroupsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+}
--- glossaryApp/controllers/GlossaryViewController.swift
+++ glossaryApp/controllers/GlossaryViewController.swift
@@ -3,10 +3,10 @@ import Firebase
 import FirebaseDatabase
 import FirebaseAuth
 
-class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,
+UICollectionViewDelegate, UICollectionViewDataSource {
     
-    var ref: DatabaseReference!
-
+    @IBOutlet weak var languageGroupCollectionView: UICollectionView!
     @IBOutlet weak var translateThisText: UITextField!
     @IBOutlet weak var firstLanguageOutlet: UIButton!
     @IBOutlet weak var secondLanguageOutlet: UIButton!
@@ -14,16 +14,34 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     @IBOutlet weak var translateButton: UIButton!
     @IBOutlet weak var languagePicker: UIPickerView!
     @IBOutlet weak var closePicker: UIButton!
-
+    @IBOutlet weak var heartButton: UIButton!
+    
+    var ref: DatabaseReference!
+    let USERS = "users"
+    let LANGUAGES = "languages"
+    var firstLanguages = [String]()
+    var secondLanguages = [String]()
+    var titleLanguages = [String]()
+    var firstCodes = [String]()
+    var secondCodes = [String]()
+    var keys = [String]()
+    
     var checkFirstLanguageButton = true
     var checkSecondLanguageButton = true
     var fL = false
     var sL = false
+    var switchChecked = true
+    var heartClicked = true
     
     var setFirstLanguage = ""
     var setSecondLanguage = ""
     var setFirstCode = ""
     var setSecondCode = ""
+    var f = ""
+    var s = ""
+    var getKey = ""
+    var wordKey = ""
+
     
     var languages = LanguageModel()
     var languageCount = 0
@@ -31,6 +49,7 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ref = Database.database().reference().child(USERS).child((Auth.auth().currentUser?.uid)!)
         
         languagePicker.delegate = self
         languagePicker.dataSource = self
@@ -38,11 +57,9 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         languagePicker.isHidden = true
         closePicker.isHidden = true
         translateButton.isHidden = false
+        heartButton.isHidden = true
     
         languageCount = languages.getLanguages().count
-        
-        print("amount of lang", languageCount)
-        
 
         navigationItem.hidesBackButton = true
         self.navigationController?.setNavigationBarHidden(false, animated: false)
@@ -64,10 +81,101 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         )
         self.navigationItem.rightBarButtonItem = addButton
         
+        
+        
+        
+        //check if user is signed in
+        if(Auth.auth().currentUser == nil){
+            performSegue(withIdentifier: "signup", sender: nil)
+        }else{
+            print("we have a user loged in!: \(String(describing: Auth.auth().currentUser!.uid))")
+            ref = Database.database().reference().child(USERS).child((Auth.auth().currentUser?.uid)!)
+            
+            //Get languages
+            ref.observe(.value, with: { (snapshot) in
+                let array:NSArray = snapshot.children.allObjects as NSArray
+                self.firstLanguages.removeAll()
+                self.secondLanguages.removeAll()
+                self.titleLanguages.removeAll()
+                
+                for child in array {
+                    let snap = child as! DataSnapshot
+                    if snap.value is NSDictionary {
+                        let data:NSDictionary = snap.value as! NSDictionary
+                        if let getLang = data.value(forKey: self.LANGUAGES) {
+                            let getLang2:NSDictionary = getLang as!
+                            NSDictionary
+                            let firstLanguage  = getLang2["firstLanguage"]
+                            let secondLanguage  = getLang2["secondLanguage"]
+                            let titleLanguage  = getLang2["title"]
+                            let firstCode  = getLang2["firstCode"] as! String
+                            let secondCode  = getLang2["secondCode"] as! String
+                            let key = snap.key
+
+                            self.keys.append(key)
+
+                            self.firstCodes.append(firstCode)
+                            self.secondCodes.append(secondCode)
+                            self.firstLanguages.append(firstLanguage as! String)
+                            self.secondLanguages.append(secondLanguage as! String)
+                            self.titleLanguages.append(titleLanguage as! String)
+                            
+                        }
+                    }
+                }
+                self.languageGroupCollectionView.reloadData()
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+        }
+        
         }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func mainTranslationAction(_ sender: Any) {
+        firstLanguageOutlet.isEnabled = true
+        secondLanguageOutlet.isEnabled = true
+        heartButton.isHidden = true
+    }
+    
+
+    @IBAction func heartButtonAction(_ sender: Any) {
+        if(heartClicked){
+            heartButton.setImage(UIImage(named: "heart filled"), for: .normal)
+            heartClicked = false
+            
+            //save to db
+            let newRef = self.ref.child(getKey).child("languages").child("wordList").childByAutoId()
+                newRef.setValue([setFirstLanguage: "hej", setSecondLanguage: "hej"])
+            
+            self.wordKey = newRef.key
+        } else{
+            heartButton.setImage(UIImage(named: "heart not filled"), for: .normal)
+            heartClicked = true
+            
+            self.ref.child(getKey).child("languages").child("wordList").child(self.wordKey).removeValue()
+        }
+        
+    }
+    
+    
+    @IBAction func languageSwitch(_ sender: Any) {
+        //should check if bool is true or false to change first language and second language
+        if(switchChecked){
+            
+            //change first language to second and vice verse
+            firstLanguageOutlet.setTitle(setSecondLanguage, for: .normal)
+            secondLanguageOutlet.setTitle(setFirstLanguage, for: .normal)
+            switchChecked = false
+        } else{
+            //go back to starting point
+            firstLanguageOutlet.setTitle(setFirstLanguage, for: .normal)
+            secondLanguageOutlet.setTitle(setSecondLanguage, for: .normal)
+            switchChecked = true
+        }
     }
     
     @IBAction func closePicker(_ sender: Any) {
@@ -75,7 +183,6 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         closePicker.isHidden = true
     }
     @objc func tapbutton(sender: UIBarButtonItem) {
-        print("you taped logout button")
         let firebaseAuth = Auth.auth()
         do {
             try firebaseAuth.signOut()
@@ -87,8 +194,6 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     }
     
     @IBAction func firstLanguageAction(_ sender: Any) {
-        print("lang check", checkFirstLanguageButton)
-        
         if(checkFirstLanguageButton){
             //firstLanguageOutlet.setTitle(languages.getLanguages()[0], for: .normal)
             //setFirstCode = languages.getCodes()[0]
@@ -105,8 +210,6 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     }
     
     @IBAction func secondLanguageAction(_ sender: Any) {
-        print("lang check", checkSecondLanguageButton)
-        
         if(checkSecondLanguageButton){
             //secondLanguageOutlet.setTitle(languages.getLanguages()[0], for: .normal)
             //setSecondCode = languages.getCodes()[0]
@@ -123,27 +226,29 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     }
     
     @IBAction func translateButtonAction(_ sender: Any) {
+        heartButton.setImage(UIImage(named: "heart not filled"), for: .normal)
+        heartClicked = true
+        
         var params = ROGoogleTranslateParams()
         let translator = ROGoogleTranslate()
-        translator.apiKey = "AIzaSyA7yKH_0jFv_rvLSvt8oCbHhk3bi9oEI0M" // Add your API Key here
-
-        params.source = setFirstCode
-        params.target = setSecondCode
+        translator.apiKey = "" // Add your API Key here
+        
+        if(switchChecked){
+            params.source = setFirstCode
+            params.target = setSecondCode
+        } else{
+            params.source = setSecondCode
+            params.target = setFirstCode
+        }
         
         params.text = translateThisText.text ?? "The textfield is empty"
         
-        print("params",  params.text)
         translator.translate(params: params) { (result) in
-            print("result", result)
-
             DispatchQueue.main.async {
-                print("result", result)
                 self.translatedText.text = "\(result)"
             }
         }
     }
-    
-    
     
     // data method to return the number of column shown in the picker.
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -152,39 +257,62 @@ class GlossaryViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     
     // data method to return the number of row shown in the picker.
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return languageCount
+            return languageCount
     }
     
     // delegate method to return the value shown in the picker
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return languages.getLanguages()[row] as? String
+            return languages.getLanguages()[row] as? String
     }
     
     // delegate method called when the row was selected.
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if(fL){
-            sL = false
-            print("row: \(row)")
-            print("value: \(languages.getLanguages()[row])")
-            firstLanguageOutlet.setTitle(languages.getLanguages()[row], for: .normal)
-            setFirstLanguage = languages.getLanguages()[row]
-            setFirstCode = languages.getCodes()[row]
-            print("code: ", setFirstCode)
-            
+            if(fL){
+                sL = false
+                firstLanguageOutlet.setTitle(languages.getLanguages()[row], for: .normal)
+                setFirstLanguage = languages.getLanguages()[row]
+                setFirstCode = languages.getCodes()[row]
 
-        }
-        
-        if(sL){
-            fL = false
-            print("row: \(row)")
-            print("value: \(languages.getLanguages()[row])")
-            secondLanguageOutlet.setTitle(languages.getLanguages()[row], for: .normal)
-            setSecondLanguage = languages.getLanguages()[row]
-            setSecondCode = languages.getCodes()[row]
-            print("code: ", setSecondCode)
-            
+                languagePicker.isHidden = true
+                closePicker.isHidden = true
+            }
+            if(sL){
+                fL = false
+                secondLanguageOutlet.setTitle(languages.getLanguages()[row], for: .normal)
+                setSecondLanguage = languages.getLanguages()[row]
+                setSecondCode = languages.getCodes()[row]
 
-        }
+                languagePicker.isHidden = true
+                closePicker.isHidden = true
+            }
     }
-
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.titleLanguages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! languageGroupsCollectionViewCell
+        cell.title.text = self.titleLanguages[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        heartButton.isHidden = false
+        firstLanguageOutlet.isEnabled = false
+        secondLanguageOutlet.isEnabled = false
+        
+        setFirstLanguage = self.firstLanguages[indexPath.row]
+        setSecondLanguage = self.secondLanguages[indexPath.row]
+        
+        firstLanguageOutlet.setTitle(setFirstLanguage, for: .normal)
+        secondLanguageOutlet.setTitle(setSecondLanguage, for: .normal)
+        
+        setFirstCode = self.firstCodes[indexPath.row]
+        setSecondCode = self.secondCodes[indexPath.row]
+        
+        getKey = keys[indexPath.row]
+    }
+    
 }
